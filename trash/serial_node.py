7947741c6c37dd654d76650b1c1690353d1ab328
Sagar Chaudhary <078bei031.sagar@pcampus.edu.np>
2024-01-12 00:28:18 +0545

    Minor change

--- trash/serial_node.py
+++ trash/serial_node.py
@@ -10,18 +10,18 @@ import crc8
 START_BYTE = 0xA5
 BLACK_TTL = '/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0'
 RED_TTL = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0'
-USING_TTL = RED_TTL
+USING_TTL = BLACK_TTL
 
 class SerialNode(Node):
     def __init__(self):
         super().__init__('serial_node')
         self.state_publisher_ = self.create_publisher(Float32MultiArray, '/raw_robot_state', 10)
-        self.cmd_subscription = self.create_subscription(Float32MultiArray, '/cmd_robot_vel', self.joy_callback, 10)
+        self.cmd_subscription = self.create_subscription(Float32MultiArray, '/cmd_robot_vel', self.cmd_callback, 10)
         self.get_logger().info('Serial node is running...')
         self.serial_port = serial.Serial(USING_TTL, 115200)
         self.timer = self.create_timer(0.005, self.odom_serial_receive)
 
-    def joy_callback(self, msg):
+    def cmd_callback(self, msg):
         # print(msg.data)
         data = [bytes(struct.pack("B", START_BYTE)),
                 bytes(struct.pack("f", msg.data[0])),
@@ -45,8 +45,8 @@ class SerialNode(Node):
                     # print("new data")
                     data_str = self.serial_port.read(25)
                     start_byte_found = True
-                else:
-                    print("START_BYTE_NOT_FOUND")
+                # else:
+                #     print("START_BYTE_NOT_FOUND")
             hash = self.rx_calc_crc(data_str)
             if hash == data_str[-1]:
                 self.serial_port.reset_input_buffer()
